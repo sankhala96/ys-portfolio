058e3e301bd0e9f899aa90fa4a9f831e66e469b6
sankhala96 <sankhalayash96@gmail.com>
2020-08-09 21:02:34 +0530

    Updates

---
+++ precache-manifest.8b3278729a18d01214da72ebe0c46813.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4afc77e1a4491ba28246cc5f590022",
+    "url": "/ys-portfolio/index.html"
+  },
+  {
+    "revision": "c1d5ab7d2c23162c32b0",
+    "url": "/ys-portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a534e03be313ae7bb97f",
+    "url": "/ys-portfolio/static/css/main.935a93ae.chunk.css"
+  },
+  {
+    "revision": "c1d5ab7d2c23162c32b0",
+    "url": "/ys-portfolio/static/js/2.e727aa75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ys-portfolio/static/js/2.e727aa75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a534e03be313ae7bb97f",
+    "url": "/ys-portfolio/static/js/main.af4fdb71.chunk.js"
+  },
+  {
+    "revision": "27c4bed1f1f2b8f7dbcc",
+    "url": "/ys-portfolio/static/js/runtime-main.a60cc212.js"
+  }
+]);
